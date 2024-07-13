c7b5872a0617ae3aea319a4c3d3057b736ff39aa
mustafayumrutepe <mstfyumrutepe@gmail.com>
2024-07-13 18:52:33 +0300

    Added logging stuff to BaseDriver and library

--- src/mustafa/test1.java
+++ src/mustafa/test1.java
@@ -1,2 +1,9 @@
-package mustafa;public class test1 {
+package mustafa;
+import org.testng.annotations.Test;
+import utility.BaseDriver;
+public class test1 extends BaseDriver {
+    @Test
+    public void test1(){
+
+    }
 }
--- src/utility/BaseDriver.java
+++ src/utility/BaseDriver.java
@@ -1,16 +1,27 @@
+package utility;
+
+import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import java.time.Duration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BaseDriver {
-
+    public static final org.apache.logging.log4j.Logger logger4j= LogManager.getLogger();
     public static WebDriver driver;
     public static WebDriverWait wait;
 
-    static {
+    @BeforeClass
+    public void startupOperations() {
         Logger logger=Logger.getLogger("");
         logger.setLevel(Level.SEVERE);
 
@@ -18,13 +29,15 @@ public class BaseDriver {
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
-
         wait=new WebDriverWait(driver,Duration.ofSeconds(20));
 
-        driver.get("https://demo.openmrs.org/");
+        loginTest();
     }
-    public static void WaitAndQuit(){
+
+
+
+    @AfterClass
+    public static void endOperations(){
         OptionalWait(5);
         driver.quit();
     }
@@ -36,8 +49,14 @@ public class BaseDriver {
             throw new RuntimeException(e);
         }
     }
-}
 
+    public void loginTest(){
+        driver.get("https://demo.openmrs.org/");
+        OptionalWait(2);
+    }
+
+}
+//TODO BURAYA SONRA BEFOREMETHOD VE AFTERMETHOD ILE LOGLAMA ISLEMLERINI YAP
 
 /*
 Kullanacağımız environment https://openmrs.org adresidir. Bu environmentin alt alanı
