5322882829fed40973c3b943c6b449bad4476a2d
kudencagri <Kudencagri@gmail.com>
2024-07-15 15:06:08 +0300

    branche has been fixed

--- src/cagri/test2.java
+++ src/cagri/test2.java
@@ -2,7 +2,7 @@ package cagri;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("meraba");
-        System.out.println("meraba");
+        System.out.println("dsadsa");
     }
+
 }
