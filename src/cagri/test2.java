4937c6421449e5ce6c7218b84e347d005d7850ed
kudencagri <Kudencagri@gmail.com>
2024-07-24 13:25:58 +0300

    deneme 1

--- src/cagri/test2.java
+++ src/cagri/test2.java
@@ -1,9 +1,9 @@
 package cagri;
 
-public class test2 {
-    public static void main(String[] args) {
-        System.out.println("dsadsa");
-        System.out.println("hghghf");
+public class test2  extends mustafa.Elements{
+    public static void main(String[] args)  {
+
+
     }
 
 }
